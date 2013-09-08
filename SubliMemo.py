db88c70eb791b7c177316c27c6490995ace25d6e
Tatsuji Ashitani <ashipong@gmail.com>
2013-09-08 19:07:54 +0900

    refactored

--- SubliMemo.py
+++ SubliMemo.py
@@ -1,8 +1,51 @@
 # -*- coding: utf-8 -*-
 import sublime, sublime_plugin
 import os
+import fnmatch
+import datetime
 
-class GotoSubliMemoCommand(sublime_plugin.WindowCommand):
+class SubliMemoBase:
+	def __init__(self):
+		self.settings=sublime.load_settings("SubliMemo.sublime-settings")
+		settings = self.settings
+		self.path = self.settings.get("path")
+
+		if ((self.path == None) or (self.path=="")):
+			self.path = ""
+			sublime.error_message("Prease set path preferly\n(Preferences-Package Settings-SubliMemo)")
+			return
+		self.path+="/"
+		try:
+			self.file_list = os.listdir(self.path)
+			self.file_list = fnmatch.filter(self.file_list,'*.txt')
+		except FileNotFoundError:
+			sublime.error_message("Prease set path preferly.\n(Preferences-Package Settings-SubliMemo)")
+			return
+		self.file_list.sort()
+		self.file_list.reverse()
+
+class CreateSubliMemoCommand(sublime_plugin.TextCommand, SubliMemoBase):
+	
+	def __init__(self,args):
+		self.args=args
+		SubliMemoBase.__init__(self)
+		sublime_plugin.TextCommand.__init__(self,args)
+
+	def on_done(self,word):
+		date = datetime.datetime.today().strftime("%Y%m%d_")		
+		filename = self.path + date + word + ".txt"
+		v = self.view.window().open_file(filename)
+
+	def run(self,edit):
+		self.view.window().show_input_panel("Memo Name:", 
+			"", self.on_done, None,None)
+
+class GotoSubliMemoCommand(sublime_plugin.WindowCommand, SubliMemoBase):
+
+	def __init__(self,args):
+		self.args=args
+		SubliMemoBase.__init__(self)
+		sublime_plugin.WindowCommand.__init(self,args)
 
 	def on_select(self, idx):
 		if idx < 0:
@@ -11,18 +54,4 @@ class GotoSubliMemoCommand(sublime_plugin.WindowCommand):
 		self.window.open_file(self.path+self.file_list[idx])
 
 	def run(self):
-
-		self.settings=sublime.load_settings("SubliMemo.sublime-settings")
-		settings = self.settings
-		self.path = self.settings.get("path")
-
-		if ((self.path == None) or (self.path=="")):
-			self.path = ""
-			sublime.error_message("Prease set path\n(Preferences-Package Settings-SubliMemo)")
-			return
-		
-		self.path+="/"
-		self.file_list = os.listdir(self.path)
-		self.file_list.sort()
-		self.file_list.reverse()
 		sublime.active_window().show_quick_panel(self.file_list, self.on_select)
